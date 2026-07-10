d14e6050f0d4f8da87660cad7c4e545e717109c6
Bibek Yadav <bibekyadav735@gmail.com>
2026-07-10 23:48:10 +0545

    The E-Commerce Inventory Manager

---
+++ The E-Commerce Inventory Manager/main.js
@@ -0,0 +1,24 @@
+const inventory = []
+
+function createProduct(name, price, quantity) {
+    return { name, price, quantity };
+}
+
+const item1 = createProduct("Laptop", 1200, 10);
+console.log(item1);
+
+const item2 = createProduct("Mouse", 25, 50);
+console.log(item2);
+
+const item3 = createProduct("Keyboard", 100, 20);
+console.log(item3);
+
+inventory.push(item1, item2, item3);
+
+inventory[1].price = 30;
+
+console.log(inventory);
+
+inventory[0].category = "Electronics";
+console.log(inventory);
+
